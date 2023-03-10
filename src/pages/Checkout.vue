860c6c330fbfd972b88959f736d8d24c5d670fb9
soyUnaGata <fallen.snitch@gmail.com>
2023-03-10 16:33:46 +0200

    add style of shipping

--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -93,6 +93,26 @@
     </div>
    </div>
 </template>
+  
+<script>
+  import { defineComponent } from 'vue';
+  import Cart from 'components/Cart.vue';
+  import CountDown from 'src/components/CountDown.vue';
+  import AddressForm from 'src/components/AddressForm.vue';
+  import CardPaymentMethodForm from 'src/components/CardPaymentMethodForm.vue';
+  import ExpressCheckout from 'src/components/ExpressCheckout.vue';
+  
+  export default defineComponent({
+    name: 'PageCheckout',
+
+    components: {
+        CountDown,
+        AddressForm,
+        CardPaymentMethodForm,
+        Cart,
+    },
+  })
+</script> 
 
 <style>
 .checkout__wrapper{
@@ -110,7 +130,6 @@
     width: 898px;
     background-color: var(--bg--cart);
 }
-
 .shipping{
   display: flex;
   align-items: flex-end;
@@ -129,19 +148,209 @@
   display: flex;
   flex-direction: column;
 }
-</style>
-  
-<script>
-  import { defineComponent } from 'vue';
-  import Shipping from 'components/Shipping.vue';
-  import Cart from 'components/Cart.vue';
-  
-  export default defineComponent({
-    name: 'PageCheckout',
+.shipping__wrapper{
+  width: 540px;
+  display: flex;
+  flex-direction: column;
+}
+.free-shipping-radio-fake, .dhl-shipping-radio-fake, .same-shipping-radio-fake, .other-shipping-radio-fake{
+  border: 1px solid #D6D8EE;
+  height: 24px;
+  width: 24px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.free-shipping input[type="radio"]:checked + .free-shipping-radio-fake::after{
+  content: '';
+  display: flex;
+  height: 12px;
+  width: 12px; 
+  background-color: #4B4E68;
+  border-radius: 50%;
+}
+.dhl-shipping input[type="radio"]:checked + .dhl-shipping-radio-fake::after{
+  content: '';
+  display: flex;
+  height: 12px;
+  width: 12px; 
+  background-color: #4B4E68;
+  border-radius: 50%;
+}
+.same-shipping input[type="radio"]:checked + .same-shipping-radio-fake::after{
+  content: '';
+  display: flex;
+  height: 12px;
+  width: 12px; 
+  background-color: #4B4E68;
+  border-radius: 50%;
+}
+.other-shipping input[type="radio"]:checked + .other-shipping-radio-fake::after{
+  content: '';
+  display: flex;
+  height: 12px;
+  width: 12px; 
+  background-color: #4B4E68;
+  border-radius: 50%;
+}
+.breadcrumbs__h1{
+  margin-top: 60px;
+}
+h2{
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 46px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: var(--primary--dark);
+}
+.breadcrumbs__cart{
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 24px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: var(--primary--dark) !important;
+}
+.breadcrumbs__cart-link{
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 24px;
+  text-decoration: none;
+  color:var(--light--grey) !important;
+}
+.contanct__info{
+  margin-top: 50px;
+}
+.contact__info-text{
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 34px;
+  color: var(--primary--dark);
+}
+.email__address__input{
+  margin-top: 29px;
+}
+.shipping__address{
+  margin-top: 50px;
+}
+.shipping__address-text{
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 34px;
+}
+.shipping__method{
+  margin-top: 50px;
+  color: #000034;
+}
+.shipping__method-text{
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 34px;
+  color: #000034;
+}
+.check-shipping__method{
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  color: #4B4E68;
+  display: flex;
+  padding-top: 20px;
+}
+.free-shipping, .dhl-shipping{
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  cursor: pointer;
+}
+.shipping__method-price{
 
-    components: {
-        Shipping,
-        Cart,
-    },
-  })
-</script> 
+  display: flex;
+}
+.dhl-shipping-price{
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 20px;
+}
+.shipping__cost{
+  font-weight: 600;
+  font-size: 36px;
+  text-align: right;
+  color: #000034;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  width: 50px;
+}
+.payment__method{
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+.payment-text{
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 34px;
+  color: #000034;
+}
+.payment__method > span{
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 21px;
+  color: #84849A;
+}
+
+.billing__address{
+  margin-top: 50px;
+}
+.billing-text{
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 34px;
+}
+.billing__address > span{
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 21px;
+  color: #84849A;
+}
+.check-billing__address{
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 20px;
+}
+.same-shipping,.other-shipping{
+  display: flex;
+  gap: 10px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  color: #4B4E68;
+  cursor: pointer;
+}
+.complete__order{
+  display: flex;
+  padding: 16px 40px;
+  gap: 6px;
+  height: 56px;
+  background: #050824;
+  border-radius: 100px;
+  color: var(--bg--shipping);
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 24px;
+  cursor: pointer;
+  border: none;
+  align-items: center;
+  margin-top: 50px;
+  width: 257px;
+}
+.complete__order:disabled{
+  background-color: rgba(5, 8, 36, 0.3);
+}
+</style>
